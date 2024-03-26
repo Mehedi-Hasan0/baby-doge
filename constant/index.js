553bb160b6040edeaabece06521af2f433f328e8
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2024-03-27 03:38:37 +0600

    [Add] footer section added

--- constant/index.js
+++ constant/index.js
@@ -24,3 +24,67 @@ export const navLinks = [
     link: "/affiliate",
   },
 ];
+
+export const footerData = {
+  about: [
+    {
+      label: "About NFT",
+      link: "#",
+    },
+    {
+      label: "Live Actions",
+      link: "#",
+    },
+    {
+      label: "NFT Blog",
+      link: "#",
+    },
+    {
+      label: "Activity",
+      link: "#",
+    },
+  ],
+  support: [
+    {
+      label: "Help & Support",
+      link: "#",
+    },
+    {
+      label: "Item Details",
+      link: "#",
+    },
+    {
+      label: "Author Profile",
+      link: "#",
+    },
+    {
+      label: "Collection",
+      link: "#",
+    },
+  ],
+  desc1:
+    "KATANA INU IS A A PROJECT FROM CHAINVISION GAMES Nothing on this website constitutes financial advice, and it is always recommended to consult a qualified financial advisor before participating in any token or NFT purchases.",
+  desc2:
+    "Buy Katana Inu NFTs only from katanainu.com or kainu.io subdomains to be safe. Check the website's address on our main website www.katanainu.com and go to the Minting NFTs section to verify. Ask in our telegram or discord channel for confirmation. Avoid other websites!",
+
+  rights: "© All Right Reserved, mint.katanainu.com",
+  logo: "/assets/images/katanaLogo.webp",
+  socialLink: [
+    {
+      icon: "/assets/icons/youtube.svg",
+      link: "#",
+    },
+    {
+      icon: "/assets/icons/twitter.svg",
+      link: "#",
+    },
+    {
+      icon: "/assets/icons/facebook.svg",
+      link: "#",
+    },
+    {
+      icon: "/assets/icons/google.svg",
+      link: "#",
+    },
+  ],
+};
